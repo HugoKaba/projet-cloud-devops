f2f5faed10765a51d0ccc3d2a15eb6352c88c608
HugoKaba <hugo.kaba@gmail.com>
2025-07-24 10:15:34 +0200

    fix style secrets.tf

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,6 +1,6 @@
 resource "random_id" "secret_suffix" {
   byte_length = 4
-  
+
   keepers = {
     version = "v1"
   }
@@ -21,12 +21,12 @@ resource "aws_secretsmanager_secret" "app_secrets" {
 resource "aws_secretsmanager_secret_version" "app_secrets" {
   secret_id = aws_secretsmanager_secret.app_secrets.id
   secret_string = jsonencode({
-    database_url    = "dynamodb://${var.aws_region}/${aws_dynamodb_table.app_table.name}"
-    database_table  = aws_dynamodb_table.app_table.name
-    environment     = "production"
-    region          = var.aws_region
-    instance_id     = aws_instance.app_server.id
-    api_endpoint    = "http://${aws_eip.app_eip.public_ip}:3001"
+    database_url   = "dynamodb://${var.aws_region}/${aws_dynamodb_table.app_table.name}"
+    database_table = aws_dynamodb_table.app_table.name
+    environment    = "production"
+    region         = var.aws_region
+    instance_id    = aws_instance.app_server.id
+    api_endpoint   = "http://${aws_eip.app_eip.public_ip}:3001"
   })
 }
 
